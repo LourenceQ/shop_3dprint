32426a6259f02b9a7448e763489435709546f274
Louernce <lqfmecatronica@gmail.com>
2020-11-26 23:33:42 -0300

    model category

--- shop_project/shop_app/models.py
+++ shop_project/shop_app/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Category(models.Model):
     name = models.charField(max_length=200,deb_inex=200);
     slug = models.SlugField(,ax_length=200, unique=True);
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.return
