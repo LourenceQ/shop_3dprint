4fffed226accc578d3dd98c837fc06449e4843f9
Louernce <lqfmecatronica@gmail.com>
2020-12-06 23:37:43 -0300

    setting up static files

--- shop_project/shop_app/views.py
+++ shop_project/shop_app/views.py
@@ -10,7 +10,7 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop_app/product/list.html'
+    return render(request, 'shop_app/product/list.html',
                             {'category': category,
                              'categories':categories,
                              'products':products})
