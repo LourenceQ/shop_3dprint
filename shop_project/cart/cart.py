6a9945c4cabc3a51188fe812b011fb0d43ab5255
Louernce <lqfmecatronica@gmail.com>
2020-12-11 22:54:33 -0300

    finished cart.py

--- shop_project/cart/cart.py
+++ shop_project/cart/cart.py
@@ -27,4 +27,45 @@ class Cart(object):
         self.save()
 
     def save(self):
-        self.session.modified = True 
+        # marca a seção como modificada e certifica que foi salva
+        self.session.modified = True
+
+    def remove(self, product):
+        """
+        Remove um produto do carrinho.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        """
+        Iteração sobre os items no carringo para receber os produtos da base de dados.
+        """
+        product_ids = self.cart.keys()
+        # pega os objetos do produto e adiciona ao carrinho
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+        def __len__(self):
+            """
+            Conta todos os items no carrinho.
+            """
+            return sum(item['quantity'] for item in self.cart.values())
+
+        def get_total_price(self):
+            return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+        def clear(self):
+            # remove o carrinho da seção
+            del self.session[settings.CART_SESSION_ID]
+            self.save()
