87eaf64dc651dec99a26ba9eae3b63036c6d2936
Louernce <lqfmecatronica@gmail.com>
2020-12-13 00:08:18 -0300

    correction in shop_app's model

--- shop_project/shop_app/models.py
+++ shop_project/shop_app/models.py
@@ -36,4 +36,4 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+        return reverse('shop_app:product_detail', args=[self.id, self.slug])
--- shop_project/shop_app/urls.py
+++ shop_project/shop_app/urls.py
@@ -6,5 +6,5 @@ app_name = 'shop_app'
 urlpatterns = [
     path('',views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-    path('<int:id>/slug:slug>/', views.product_detail,name='product_detail'),
+    path('<int:id>/<slug:slug>/', views.product_detail,name='product_detail'),
 ]
