c581ed74bfec630caa5b05394ec87c6d53a37421
Lourence <lqfmecatronica@gmail.com>
2020-12-18 06:00:33 -0300

    cart detail.html

--- shop_project/cart/views.py
+++ shop_project/cart/views.py
@@ -23,3 +23,7 @@ def cart_remove(request, product_id):
     product = get_object_or_404(product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html',{'cart'}:cart})
