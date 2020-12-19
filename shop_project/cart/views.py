31b5dd31c6145f97511c7681b56b39465308be43
Lourence <lqfmecatronica@gmail.com>
2020-12-18 23:09:41 -0300

    changing products quantities

--- shop_project/cart/views.py
+++ shop_project/cart/views.py
@@ -26,4 +26,8 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+    for ite in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={
+                             'quantity': item['quantity'],
+                             'override':True})
     return render(request, 'cart/detail.html',{'cart':cart})
--- shop_project/shop_app/views.py
+++ shop_project/shop_app/views.py
@@ -25,6 +25,6 @@ def product_detail(request, id, slug):
                                          available=True)
     cart_product_form = CartAddProductForm()
     return render(request,
-                  'shop/product/detail.html',
+                  'shop_app/product/detail.html',
                   {'product':product,
                    'cart_product_form':cart_product_form})
