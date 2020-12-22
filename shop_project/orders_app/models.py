8458c963345b5768341148a73a3955097f43954c
Lourence <lqfmecatronica@gmail.com>
2020-12-21 22:54:40 -0300

    orders

--- shop_project/orders_app/models.py
+++ shop_project/orders_app/models.py
@@ -18,8 +18,9 @@ class Order(models.Model):
 
     def __str__(self):
         return f'Order {self.id}'
-        def get_total_cost(self):
-            return sum(item.get_cost() for item in self.items.all())
+        
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
 
 
 class OrderItem(models.Model):
@@ -27,9 +28,9 @@ class OrderItem(models.Model):
                               related_name='items',
                               on_delete=models.CASCADE)
     Product = models.ForeignKey(Product,
-                                realted_name='order_items',
+                                related_name='order_items',
                                 on_delete=models.CASCADE)
-    price = nodels.DecimalField(max_digits=10, decial_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
