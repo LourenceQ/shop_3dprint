c51e3b0acf8f310b19d0feae69a71a7b402a0eda
Lourence <lqfmecatronica@gmail.com>
2020-12-22 23:17:22 -0300

    debugging

--- shop_project/cart/views.py
+++ shop_project/cart/views.py
@@ -13,20 +13,20 @@ def cart_add(request, product_id):
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product,
-                quantity=cd['quantity'],
-                override_quantity=cd['override'])
+                 quantity=cd['quantity'],
+                 override_quantity=cd['override'])
     return redirect('cart:cart_detail')
 
 @require_POST
 def cart_remove(request, product_id):
     cart = Cart(request)
-    product = get_object_or_404(product, id=product_id)
+    product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
 
 def cart_detail(request):
     cart = Cart(request)
-    for ite in cart:
+    for item in cart: # <!--//////////-->
         item['update_quantity_form'] = CartAddProductForm(initial={
                              'quantity': item['quantity'],
                              'override':True})
